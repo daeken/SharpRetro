8a9680bdf0f5b5df934b65ac975e34bacdca9705
Sera Brocious (Daeken) <sera.brocious@gmail.com>
2022-01-05 00:01:25 -0500

    Added stubs to allow PSX recompiler to compile.

--- SharpStationCore/Recompiler.cs
+++ SharpStationCore/Recompiler.cs
@@ -14,4 +14,12 @@ public partial class Recompiler : BaseCpu {
 	}
 
 	void ThrowCpuException(ExceptionType etype, uint pc, uint insn) => throw new CpuException(etype, pc, insn);
+	
+	void Branch(uint pc) {}
+	void Branch(IRuntimeValue<uint> pc) {}
+	
+	void AbsorbMuldivDelay() {}
+	
+	void MulDelay(IRuntimeValue<uint> a, IRuntimeValue<uint> b, bool signed) {}
+	void DivDelay() {}
 }
