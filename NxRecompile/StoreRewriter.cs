dd25134f38be29e8bb606521df24f94403813606
Sera Brocious <sera.brocious@gmail.com>
2025-12-04 10:24:39 -0500

    Remove single-use Stores

--- NxRecompile/StoreRewriter.cs
+++ NxRecompile/StoreRewriter.cs
@@ -19,16 +19,23 @@ public partial class CoreRecompiler {
             stores[store] = (name, new StaticIRValue.Named(name, value.Type));
         });
         if(stores.Count == 0) return stmts;
+        var usage = stores.Select(x => (x.Key, 0)).ToDictionary();
+        stmts.WalkValues(value => {
+            if(value is StaticIRValue.Store store) usage[store]++;
+        });
+        foreach(var (store, count) in usage)
+            if(count == 1)
+                stores.Remove(store);
         var assigned = new HashSet<string>();
         stmts = stmts.Transform(stmt => {
             if(stmt is IStaticControlFlowStatement) return null;
             var containsStore = false;
-            stmt.WalkValues(value => { if(value is StaticIRValue.Store store) containsStore = true; });
+            stmt.WalkValues(value => { if(value is StaticIRValue.Store store && stores.ContainsKey(store)) containsStore = true; });
             if(!containsStore) return null;
             var assignments = new List<StaticIRStatement>();
             stmt = stmt.TransformValues(value => {
                 if(value is not StaticIRValue.Store store) return null;
-                if(!stores.TryGetValue(store, out var temp)) return null;
+                if(!stores.TryGetValue(store, out var temp)) return store.Value;
                 var (name, named) = temp;
                 if(assigned.Add(name))
                     assignments.Add(new StaticIRStatement.Assign(name, store.Value));
