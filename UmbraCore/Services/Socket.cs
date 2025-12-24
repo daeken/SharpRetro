14def8028e0dd2252c8257f02f282820d3450e69
Sera Brocious <sera.brocious@gmail.com>
2025-12-23 19:53:42 -0500

    Added socket stubs

---
+++ UmbraCore/Services/Socket.cs
@@ -0,0 +1,12 @@
+using UmbraCore.Core;
+// ReSharper disable once CheckNamespace
+namespace UmbraCore.Services.Nn.Socket.Sf;
+
+public partial class IClient {
+    protected override uint RegisterClient(BsdBufferConfig config, ulong pid,
+        ulong transferMemorySize, KObject transferMemory, ulong _4
+    ) {
+        Console.WriteLine($"Nn.Socket.Sf.IClient.RegisterClient pid=0x{pid:X} transferMemorySize=0x{transferMemorySize:X} transferMemory=0x{transferMemory?.Handle} _4=0x{_4:X}");
+        return 0;
+    }
+}
